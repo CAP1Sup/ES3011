1e5ce9d8d0e59a70b356a4a1dcf5ebafa022b25f
CAP1Sup <xmanpiper@gmail.com>
2024-01-22 21:29:09 -0500

    Added lab1 q3

---
+++ lab1/q3.m
@@ -0,0 +1,19 @@
+%% Initialize the system constants
+m = 1500; % kg
+b = 50; % Ns/m
+F = 1500; % N
+
+%% Set up state space matrices
+A = [0 1; 0 -b/m];
+B = [0; 1/m];
+C = [0 1];
+D = [0];
+
+%% Set up state space equations
+sys = ss(A, B, C, D);
+
+%% Simulate the model
+t = [0 : 0.05 : 100];
+u = F * ones(size(t));
+lsim(sys, u, t)
+axis([0 100 0 30])
